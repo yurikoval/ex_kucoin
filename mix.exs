28cf82dc67014a44024a40c7411106cc5f43e90f
Yuri Kovalov <yuri@yurikoval.com>
2019-05-10 16:37:59 +0900

    Bump to v0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExKucoin.MixProject do
   def project do
     [
       app: :ex_kucoin,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
