43a5bf7c1e907336d7181a9158c964b9c1dafefa
Yuri Kovalov <yuri@yurikoval.com>
2019-05-10 15:12:04 +0900

    Create sell order

---
+++ test/trade/order_test.exs
@@ -0,0 +1,40 @@
+defmodule ExKucoin.Trade.OrderTest do
+  import TestHelper
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+  alias ExKucoin.Trade.Order, as: Api
+
+  setup_all do
+    config = %ExKucoin.Config{
+      api_key: System.get_env("OKEX_API_KEY") || "XXX",
+      api_secret: System.get_env("OKEX_API_SECRET") || "XXX",
+      api_passphrase: System.get_env("OKEX_API_PASSPHRASE") || "XXX",
+      api_url: "https://openapi-sandbox.kucoin.com"
+    }
+
+    %{config: config}
+  end
+
+  describe ".create" do
+    test "creates market sell order", %{config: config} do
+      params = %{
+        "clientOid" => "XXXXX",
+        "side" => "sell",
+        "symbol" => "KCS-BTC",
+        "type" => "market",
+        "size" => 1
+      }
+
+      filter_sensitive_data()
+
+      use_cassette "trade/create_sell_order" do
+        assert {:ok, detail} = Api.create(params, config)
+
+        assert %{
+                 "code" => "200000",
+                 "data" => %{"orderId" => _order_id}
+               } = detail
+      end
+    end
+  end
+end
