043830f28c30df4f9cb179fd0641f492218cf22d
Yuri Kovalov <yuri@yurikoval.com>
2021-09-15 14:38:36 +0800

    Release v0.0.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExKucoin.MixProject do
   def project do
     [
       app: :ex_kucoin,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
